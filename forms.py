a1f284db6c8cf963697dfcf227f279749aca2315
Akhila1019 <chitneediakhila@gmail.com>
2022-03-17 09:36:03 +0530

    Added pancard related forms

--- forms.py
+++ forms.py
@@ -1,10 +1,26 @@
 from flask_wtf import Form,RecaptchaField
 from wtforms import StringField,SubmitField,SelectField,RadioField,BooleanField,PasswordField
-from wtforms.validators import DataRequired,InputRequired,Length,EqualTo
+from wtforms.validators import DataRequired,InputRequired,Length,EqualTo,ValidationError
+from models import Credentials,Registration
+
+def DataNotExists(param):
+        def _DataNotExists(form,field):
+            msg = 'msg'
+            if param=='Account number':
+                msg = Registration.query.filter_by(account_number=field.data).first()
+            if param == 'CIF number':
+                msg = Registration.query.filter_by(cif_number=field.data).first()
+            
+            if param == 'Username':
+                msg = Credentials.query.filter_by(username=field.data).first()
+            if msg:
+                raise ValidationError(param+" Already exists!")
+        return _DataNotExists
+
 
 class SignupForm(Form):
-    accno = StringField('Account Number',validators=[DataRequired()])
-    cifno = StringField('CIF Number',validators=[DataRequired()])
+    accno = StringField('Account Number',validators=[DataRequired(),DataNotExists('Account number')])
+    cifno = StringField('CIF Number',validators=[DataRequired(),DataNotExists('CIF number')])
     branchcode = StringField('Branch code',validators=[DataRequired()])
     country = SelectField('Country',validators=[DataRequired()], choices=['India','Pakistan'])
     phone = StringField('Phone Number',validators=[DataRequired()])
@@ -23,13 +39,36 @@ class ATMForm(Form):
     submit = SubmitField('Submit')
 
 class UserForm(Form):
-    username = StringField('Enter new username',validators=[DataRequired('Please enter username'),Length(min=6,max=20,message="Enter valid username")])
+    username = StringField('Enter new username',validators=[DataRequired('Please enter username'),Length(min=6,max=20,message="Enter valid username"),DataNotExists('Username')])
     password = PasswordField('Enter new Login Password',validators=[DataRequired('Please enter password'),Length(min=6,max=20,message="Password must be 6 characters or more")])
     confirm_password = PasswordField('Confirm New Login Password',validators=[DataRequired(message='Please enter password'),EqualTo('password', message='Both password fields must be equal!')])
     checkbox = BooleanField('I accept the Terms and Conditions',validators=[DataRequired()])
     submit = SubmitField('Submit')
     reset = SubmitField('Reset')
 
+def ValidPassword(other):
+    def _ValidPassword(form,field):
+        username = form[other].data
+        user = Credentials.query.filter_by(username = username).first()        
+        if user is not None and not user.check_password(field.data):
+            raise ValidationError("Wrong Password")
+    return _ValidPassword
+
 class PancardForm(Form):
-    password = PasswordField('Enter password',validators=[DataRequired("Please enter your password.")])
+    username = StringField()
+    password = PasswordField('Enter password',validators=[DataRequired("Please enter your password."),ValidPassword(other='username')])
     submit = SubmitField('Submit')
+
+    
+
+    
+                
+
+class EnterPanForm(Form):
+    pan = StringField('PAN',validators=[DataRequired("Please enter your PAN number.")])
+    repan = PasswordField('Re-enter PAN',validators=[DataRequired("Please re-enter your PAN number."),EqualTo('pan', message='Both pan number fields must be equal!')])
+    submit = SubmitField('Submit')
+
+
+
+        
