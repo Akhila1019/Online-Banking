cbf904bc917d46f8d69f642a02acaa67cf6370b8
Akhila1019 <chitneediakhila@gmail.com>
2022-05-14 09:57:10 +0530

    made changes

--- routes.py
+++ routes.py
@@ -16,7 +16,7 @@ from flask_sqlalchemy import SQLAlchemy
 # Create instance from Flask class
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:dbpassword@localhost:5432/Online-Banking'
